41d2d4151144922a5e52a56a7c3984cf109a99b5
phpxbn <1750996630@qq.com>
2014-08-11 14:24:33 +0800

    Create Memory.php

---
+++ library/Zend/Memory/Memory.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * 2014.8.11 Written by XBN.
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace Zend\Memory;
+
+use Zend\Cache\StorageFactory;
+
+/**
+ * Memory
+ *
+ * Memory Manager factory
+ */
+class Memory {
+	
+	/**
+	 * Memory Manager Factory。
+	 *
+	 * @param String $file        	
+	 * @param Array $option        	
+	 * @return \Zend\Memory\MemoryManager *
+	 */
+	public static function factory($file = "None", $option = null) {
+		
+		if (strtolower ( $file ) === "file") {
+			if (! (is_array ( $option ) && isset ( $option ['cache_dir'] ) && file_exists ( $option ['cache_dir'] ))) {
+				$option = array (
+						'cache_dir' => './data/cache/' 
+				);
+			}
+			
+			if (! file_exists ( './data/cache/' )) {
+				mkdir ( './data/cache/' );
+			}
+			
+			$cache = StorageFactory::factory ( array (
+					'adapter' => array (
+							'name' => 'filesystem',
+							'options' => $option 
+					),
+					'plugins' => array (
+							'exception_handler' => array (
+									'throw_exceptions' => false 
+							),
+							'Serializer' 
+					) 
+			) );
+		}else{
+			$cache = null;
+		}
+		
+		return new MemoryManager ( $cache );
+	}
+}
+
+?>
